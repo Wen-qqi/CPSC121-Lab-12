761a518ef7939116cfdb5a86d315ee75325ebd7b
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-23 15:18:39 -0700

    second commit, added header, created astronaut class

--- prob02/astronaut.h
+++ prob02/astronaut.h
@@ -1,9 +1,8 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Wen Fan
+// CPSC 121L - 11
+// April 29, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 //
 // Lab 12-2
 // If it is a pair programming lab please specify partner below.
