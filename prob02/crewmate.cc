d5ecc2c5c8df0265dd484be3983228b1fd345ee1
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-23 15:20:40 -0700

    first commit, added header, finished class implmentations

--- prob02/crewmate.cc
+++ prob02/crewmate.cc
@@ -1,9 +1,8 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Wen Fan
+// CPSC 121L - 11
+// April 29, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 //
 // Lab 12-2
 // If it is a pair programming lab please specify partner below.
@@ -24,3 +23,32 @@
 //     }
 // to tell the compiler that each function belongs to the Crewmate class.
 // ===================================================================
+bool Crewmate::GetIsAlive() const { return is_alive_; }
+
+int Crewmate::GetTaskCount() const { return task_count_; }
+
+void Crewmate::SetIsAlive(bool is_alive) { is_alive_ = is_alive; }
+
+void Crewmate::DoTask(const std::string& task_name) {
+  task_count_++;
+  std::cout << Astronaut::GetName() << " is doing " << task_name << "\n";
+}
+
+graphics::Color Crewmate::GetColor() {
+  if (GetIsAlive()) {
+    return Astronaut::GetColor();
+  } else {
+    graphics::Color temp_color = Astronaut::GetColor();
+    return graphics::Color((temp_color.Red() + 256) / 2,
+                           (temp_color.Green() + 256) / 2,
+                           (temp_color.Blue() + 256) / 2);
+  }
+}
+
+std::string Crewmate::GetIconFilename() {
+  if (GetIsAlive()) {
+    return Astronaut::GetIconFilename();
+  } else {
+    return "ghost.bmp";
+  }
+}
