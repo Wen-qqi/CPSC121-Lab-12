63e58667a9a4771b4c8828fc70537a92030bdb40
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-23 15:16:37 -0700

    first commit

--- prob02/astronaut.h
+++ prob02/astronaut.h
@@ -21,5 +21,15 @@
 // ========================= YOUR CODE HERE =========================
 // Define the Astronaut class here. Refer to the README for instructions.
 // ===================================================================
+class Astronaut {
+ public:
+  Astronaut(const std::string& name, const graphics::Color& color);
+  std::string GetName() const;
+  graphics::Color GetColor() const;
+  std::string GetIconFilename() const;
 
+ private:
+  std::string name_;
+  graphics::Color color_;
+};
 #endif  // ASTRONAUT_H
