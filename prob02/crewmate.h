3b314c5d1db9d3e8ece1a0cadd0a567e9ddd49cd
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-23 15:19:42 -0700

    first commit, added header, created crewmate class

--- prob02/crewmate.h
+++ prob02/crewmate.h
@@ -1,9 +1,8 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Wen Fan
+// CPSC 121L - 11
+// April 29, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 //
 // Lab 12-2
 // If it is a pair programming lab please specify partner below.
@@ -20,5 +19,24 @@
 // Define the Crewmate class here, which inherits from the Astronaut
 // base class. Refer to the README for instructions.
 // ===================================================================
+class Crewmate : public Astronaut {
+ public:
+  Crewmate()
+      : Astronaut("no name", graphics::Color(0, 0, 0)),
+        is_alive_(true),
+        task_count_(0) {}
+  Crewmate(const std::string& name, const graphics::Color& color)
+      : Astronaut(name, color), is_alive_(true), task_count_(0) {}
+  bool GetIsAlive() const;
+  int GetTaskCount() const;
+  void SetIsAlive(bool is_alive);
+  void DoTask(const std::string& task_name);
+  graphics::Color GetColor();
+  std::string GetIconFilename();
+
+ private:
+  bool is_alive_;
+  int task_count_;
+};
 
 #endif  // CREWMATE_H
