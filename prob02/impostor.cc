dd2e7a48a647cb5215d374ac75ed7dad4c8f2c50
Wen Fan <WenFan@csu.fullerton.edu>
2024-04-23 18:56:07 -0700

    first commit, implmented impostor class, added header

--- prob02/impostor.cc
+++ prob02/impostor.cc
@@ -1,9 +1,8 @@
-// Please fill in below.
-// <Your name>
-// <Your section number> (e.g. CPSC 121L-01)
-// <Date>
-// <Your csu.fullerton.edu email>
-// <Your GitHub username>
+// Wen Fan
+// CPSC 121L - 11
+// April 29, 2024
+// WenFan@csu.fullerton.edu
+// @Wen-qqi
 //
 // Lab 12-2
 // If it is a pair programming lab please specify partner below.
@@ -26,3 +25,7 @@
 //     }
 // to tell the compiler that each function belongs to the Impostor class.
 // ===================================================================
+void Impostor::Kill(Crewmate& crewmate) const {
+  crewmate.SetIsAlive(false);
+  std::cout << GetName() << " killed " << crewmate.GetName() << "\n";
+}
